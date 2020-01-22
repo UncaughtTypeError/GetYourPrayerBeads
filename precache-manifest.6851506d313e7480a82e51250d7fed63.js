210ab646b7b6113b0d615c6804d281e6153ce5a6
Nathan Shepherd <your@email.goes.here>
2020-01-22 18:59:47 +0200

    initial build subtree commit

---
+++ precache-manifest.6851506d313e7480a82e51250d7fed63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a0b69568d6b8b72d3b16838c5f670f",
+    "url": "/GetYourPrayerBeads/index.html"
+  },
+  {
+    "revision": "bfa46cdc3460f0a1ea7e",
+    "url": "/GetYourPrayerBeads/static/css/main.f96f0d9f.chunk.css"
+  },
+  {
+    "revision": "b3f5a8764a4569a0000e",
+    "url": "/GetYourPrayerBeads/static/js/2.d6d9098a.chunk.js"
+  },
+  {
+    "revision": "bfa46cdc3460f0a1ea7e",
+    "url": "/GetYourPrayerBeads/static/js/main.966c5c27.chunk.js"
+  },
+  {
+    "revision": "6edc8e86673464428422",
+    "url": "/GetYourPrayerBeads/static/js/runtime~main.219cb577.js"
+  }
+]);
